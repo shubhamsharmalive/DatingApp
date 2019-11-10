b72c65a18bbbd078016584a12892b273db24fec5
Shubham Sharma <shubhamsharma@live.com>
2019-11-10 23:50:54 +0530

    Added changes for db connection SQL server 2017 and SSMS2018

---
+++ DatingApp/Controllers/ValueController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatingApp.Controllers
+{
+    // https://localhost:44363/api/value
+    // http://localhost:5000/api/Value
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValueController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# DatingApp
-Angular 8 with .net core 2.2
